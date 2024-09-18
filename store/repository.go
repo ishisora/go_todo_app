7921c714d7ceae205d57bfcc7b4d6bc0fc145475
ishisora <tatsu.gami2573@gmail.com>
2024-09-18 19:33:25 +0900

    DB接続時にリトライ機能を実装

--- store/repository.go
+++ store/repository.go
@@ -13,24 +13,35 @@ import (
 )
 
 func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
-	// sqlx.Connectを使うと内部でpingする
-	db, err := sql.Open("mysql",
-		fmt.Sprintf(
-			"%s:%s@tcp(%s:%d)/%s?parseTime=true",
-			cfg.DBUser, cfg.DBPassword,
-			cfg.DBHost, cfg.DBPort,
-			cfg.DBName,
-		),
-	)
+	var db *sql.DB
+	var err error
+	retryCount := 5
+	for i := 0; i < retryCount; i++ {
+		db, err = sql.Open("mysql",
+			fmt.Sprintf(
+				"%s:%s@tcp(%s:%d)/%s?parseTime=true",
+				cfg.DBUser, cfg.DBPassword,
+				cfg.DBHost, cfg.DBPort,
+				cfg.DBName,
+			),
+		)
+		if err != nil {
+			return nil, nil, err // 接続自体のエラー時にリトライしない
+		}
+		// 接続確認
+		ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+		defer cancel()
+		if err := db.PingContext(ctx); err != nil {
+			fmt.Printf("Retrying to connect to DB... (%d/%d)\n", i+1, retryCount)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		break
+	}
 	if err != nil {
 		return nil, nil, err
 	}
-	// Openは実際に接続テストが行われない
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
-	defer cancel()
-	if err := db.PingContext(ctx); err != nil {
-		return nil, func() { _ = db.Close() }, err
-	}
+
 	xdb := sqlx.NewDb(db, "mysql")
 	return xdb, func() { _ = db.Close() }, nil
 }
