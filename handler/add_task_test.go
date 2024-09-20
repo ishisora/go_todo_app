7efec98c9dfbfcf7403833df2328aa8b6483c8f9
ishisora <tatsu.gami2573@gmail.com>
2024-09-20 21:54:58 +0900

    handler.AddTask型のリファクタリングとそれに伴うテストの修正

--- handler/add_task_test.go
+++ handler/add_task_test.go
@@ -2,18 +2,18 @@ package handler
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/go-playground/validator"
 	"github.com/ishisora/go_todo_app/entity"
-	"github.com/ishisora/go_todo_app/store"
 	"github.com/ishisora/go_todo_app/testutil"
 )
 
 func TestAddTask(t *testing.T) {
-	t.Parallel()
 	type want struct {
 		status  int
 		rspFile string
@@ -48,11 +48,18 @@ func TestAddTask(t *testing.T) {
 				"/tasks",
 				bytes.NewReader(testutil.LoadFile(t, tt.reqFile)),
 			)
+			moq := &AddTaskServiceMock{}
+			moq.AddTaskFunc = func(
+				ctx context.Context, title string,
+			) (*entity.Task, error) {
+				if tt.want.status == http.StatusOK {
+					return &entity.Task{ID: 1}, nil
+				}
+				return nil, errors.New("error from mock")
+			}
 
 			sut := AddTask{
-				Store: &store.TaskStore{
-					Tasks: map[entity.TaskID]*entity.Task{},
-				},
+				Service:   moq,
 				Validator: validator.New(),
 			}
 			sut.ServeHTTP(w, r)
